af0c3cd82e2fde7fc4fdb81fd1fced71f24cc389
FreemanUAaa <rostykivankiv951@gmail.com>
2022-07-16 20:17:21 +0300

    Messages: create message for send confirm user email message

---
+++ Messages/CozaStore.Server.Messages/Messages/ISendConfirmEmailMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace CozaStore.Server.Messages.Messages;
+
+public interface ISendConfirmEmailMessage
+{
+    string UserName { get; set; }
+
+    string UserEmail { get; set; }
+
+    string ConfirmUrl { get; set; }
+}
