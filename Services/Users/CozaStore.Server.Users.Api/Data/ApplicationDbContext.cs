25063bad5d1525d2d10a068430a4a9b4337246e1
FreemanUAaa <rostykivankiv951@gmail.com>
2022-07-16 17:11:53 +0300

    Service users: init api project

---
+++ Services/Users/CozaStore.Server.Users.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using CozaStore.Server.Users.Api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CozaStore.Server.Users.Api.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
---
+++ Services/Users/CozaStore.Server.Users.Api/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CozaStore.Server.Users.Api.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
